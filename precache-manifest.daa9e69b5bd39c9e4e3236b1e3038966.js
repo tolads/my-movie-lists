977b6517460b91e09071b8ec34bf27091d45f96a
Tamas Laszlo <tamas.laszlo@lhsystems.com>
2019-09-18 12:30:25 +0200

    Updates

---
+++ precache-manifest.daa9e69b5bd39c9e4e3236b1e3038966.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175ee77dc6fbb0a4b326b958dd6693aa",
+    "url": "/my-movie-lists/index.html"
+  },
+  {
+    "revision": "33c639369754d5b45900",
+    "url": "/my-movie-lists/static/js/2.370f3196.chunk.js"
+  },
+  {
+    "revision": "9e56c26cb57c67529ee2",
+    "url": "/my-movie-lists/static/js/main.ead52e61.chunk.js"
+  },
+  {
+    "revision": "d39b649d62ccf25c80a0",
+    "url": "/my-movie-lists/static/js/runtime~main.1f4fe7f2.js"
+  }
+]);
