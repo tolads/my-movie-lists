3c3175fe0168b5650870a66b805be61cf771d32f
Tamás László <tlaszlods@gmail.com>
2019-10-31 20:01:13 +0100

    add Redux Devtools instead of Redux Logger

--- src/state/store.js
+++ src/state/store.js
@@ -1,18 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
 
 const configureStore = preloadedState => {
-  const loggerMiddleware = createLogger({
-    collapsed: true,
-  });
+  const composeEnhancers =
+    (process.env.NODE_ENV !== 'production' &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+    compose;
 
   const store = createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, loggerMiddleware),
+    composeEnhancers(applyMiddleware(thunk)),
   );
 
   if (module.hot && process.env.NODE_ENV !== 'production') {
