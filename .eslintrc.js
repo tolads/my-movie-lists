f5073ae27caf96a2fadd938caf8f33b086a95f87
Tamas Laszlo <tamas.laszlo@lhsystems.com>
2020-02-02 17:44:15 +0100

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-fragments': 'off',
+    'react/jsx-one-expression-per-line': 'off',
     'no-console': ['error', { allow: ['warn', 'error'] }],
   },
   env: {
